cf9bec2e08723ea547c574cbecee70fdb2b7bcc3
ayoubbelatrous <77838146+ayoubbelatrous@users.noreply.github.com>
2021-10-26 20:26:10 +0100

    Revert "chnages"
    This reverts commit 8668e5283312cf3ee393afea6ce8b5750355405d.

--- client/src/components/ProductPreview.vue
+++ client/src/components/ProductPreview.vue
@@ -65,7 +65,7 @@ export default {
       this.currentimg = `http://13.38.4.102/api/${this.Data.images_url}/0.png`;
       for (let index = 0; index < this.Data.imagecount; index++) {
         this.images.push(
-          `/api/${this.Data.images_url}/${index}.png`
+          `http://13.38.4.102/api/${this.Data.images_url}/${index}.png`
         );
       }
 
